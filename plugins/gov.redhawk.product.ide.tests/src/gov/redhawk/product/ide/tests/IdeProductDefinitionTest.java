6e6beed431d4256b07cbb9e4b708d40eb361006a
Ryan Bauman <ryan.bauman@axiosengineering.com>
2013-03-12 11:30:28 -0400

    Initial export of IDE product repository

---
+++ plugins/gov.redhawk.product.ide.tests/src/gov/redhawk/product/ide/tests/IdeProductDefinitionTest.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.product.ide.tests;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Tests the ide.product file to ensure custom changes are not overwritten by
+ * the Product Editor.
+ */
+public class IdeProductDefinitionTest {
+
+	private DocumentBuilder builder;
+	private List<Node> configurationElements;
+
+	/**
+	 * Parse the ide.product file and build list of items under the
+	 * configurations node.
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		this.builder = factory.newDocumentBuilder();
+		Document document = this.parseIdeProduct();
+		this.buildConfigElementList(document);
+
+	}
+
+	/**
+	 * Tear down the builder and configElements list between tests.
+	 * 
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		this.builder = null;
+		this.configurationElements = null;
+	}
+
+	/**
+	 * Tests for presence of a property element in the configuration section of
+	 * the ide.product file.
+	 */
+	@Test
+	public void isConfigurationPropertyElementPresent() {
+		Assert.assertNotNull("The ide.product file must contain a configuration/property element", this.configurationElements);
+	}
+
+	/**
+	 * Tests for presence and correct value of the eclipse.buildId property.
+	 */
+	@Test
+	public void containsEclipseBuildIdProperty() {
+		Element buildIdElement = null;
+		for (Node node : this.configurationElements) {
+			if (node instanceof Element) {
+				Element temp = (Element) node;
+				if (temp.getNodeName().equals("property") && temp.getAttribute("name").equals("eclipse.buildId")) {
+					buildIdElement = temp;
+					break;
+				}
+			}
+		}
+		Assert.assertNotNull("The ide.product file must contain an eclipse.buildId property", buildIdElement);
+		Assert.assertNotNull("The eclipse.buildId property must have a value", buildIdElement.getAttribute("value"));
+	}
+
+	/**
+	 * Parses the ide.product file.
+	 * 
+	 * @return the {@link Document} resulting from parsing the ide.product file
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	private Document parseIdeProduct() throws SAXException, IOException {
+		return this.builder.parse(FileLocator.toFileURL(FileLocator.find(Platform.getBundle("gov.redhawk.product.ide"), new Path("ide.product"), null))
+		        .toString());
+	}
+
+	/**
+	 * Builds a list of {@link Node} under the "configurations" element.
+	 * 
+	 * @param document the {@link Document} associated with the ide.product file
+	 */
+	private void buildConfigElementList(Document document) {
+		Element root = document.getDocumentElement();
+		NodeList nodes = root.getChildNodes();
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if (node instanceof Element) {
+				Element child = (Element) node;
+				if (child.getNodeName().equals("configurations")) {
+					NodeList configurationsNodes = child.getChildNodes();
+					for (int j = 0; j < configurationsNodes.getLength(); j++) {
+						if (configurationsNodes.item(j).getNodeName().equals("property")) {
+							if (this.configurationElements == null) {
+								this.configurationElements = new ArrayList<Node>();
+							}
+							this.configurationElements.add(configurationsNodes.item(j));
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ plugins/gov.redhawk.product.ide/src/gov/redhawk/product/ide/RedhawkProductIDEActivator.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.product.ide;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.LogManager;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ * @since 1.1
+ */
+public class RedhawkProductIDEActivator extends AbstractUIPlugin implements IStartup {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "gov.redhawk.product.ide";
+
+	// The shared instance
+	private static RedhawkProductIDEActivator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public RedhawkProductIDEActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		RedhawkProductIDEActivator.plugin = this;
+		
+		configureJavaLogger(context);
+	}
+
+	/**
+	 * This method ensures that if the Java logging properties weren't properly installed by the IDE's feature
+	 * into the configuration directory that we'll load a backup. This is primarily important for debugging the IDE
+	 * within Eclipse where this is the situation.
+	 * 
+	 * @param context
+	 */
+	private void configureJavaLogger(final BundleContext context) {
+	    InputStream test = null;
+		InputStream logInputStream = null;
+		try {
+			test = Platform.getConfigurationLocation().getDataArea("javalogger.properties").openStream();
+		} catch (IOException e) {
+			URL javaloggerURL = FileLocator.find(context.getBundle(), new Path("javalogger.properties"), null);
+			if (javaloggerURL != null) {
+				try {
+					logInputStream = javaloggerURL.openStream();
+					LogManager.getLogManager().readConfiguration(logInputStream);
+				} catch (IOException e2) {
+					// PASS
+				} catch (SecurityException e2) {
+					// PASS
+				} 
+			}
+		} finally {
+			if (test != null) {
+				try {
+	                test.close();
+                } catch (IOException e) {
+	                // PASS
+                }
+			}
+			if (logInputStream != null) {
+				try {
+	                logInputStream.close();
+                } catch (IOException e) {
+	                // PASS
+                }
+			}
+		}
+    }
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		RedhawkProductIDEActivator.plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static RedhawkProductIDEActivator getDefault() {
+		return RedhawkProductIDEActivator.plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(final String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin(RedhawkProductIDEActivator.PLUGIN_ID, path);
+	}
+
+	public void earlyStartup() {
+		// PASS - logic is in startup
+    }
+}
