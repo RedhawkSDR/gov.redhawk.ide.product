59ba7e01db76dddc23c4632458202516ab98e21f
Ryan Bauman <ryan.bauman@axiosengineering.com>
2013-04-16 15:18:01 -0400

    Updates to use Eclipse CBI

---
+++ tests/gov.redhawk.product.ide.tests/src/gov/redhawk/product/ide/tests/AllProductIdeTests.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * This file is protected by Copyright. 
+ * Please refer to the COPYRIGHT file distributed with this source distribution.
+ *
+ * This file is part of REDHAWK IDE.
+ *
+ * All rights reserved.  This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package gov.redhawk.product.ide.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * 
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ IdeProductDefinitionTest.class })
+public class AllProductIdeTests {
+
+}
